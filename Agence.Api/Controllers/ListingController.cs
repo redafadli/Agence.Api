0653a89ce7018f5f894f7448b5ad99c5474cb451
Reda Fadli <fadlireda13@gmail.com>
2023-04-01 12:49:27 +0200

    clean architucture

--- Agence.Api/Controllers/ListingController.cs
+++ Agence.Api/Controllers/ListingController.cs
@@ -1,10 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
 namespace Agence.Api.Controllers {
-    public class ListingController {
+    [Route("(controller")]
+    [ApiController]
+    public class ListingController : Controller {
+
         public ListingController() {
 
         }
-
-
     }
 }
+
